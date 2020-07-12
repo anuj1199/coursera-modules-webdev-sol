a3bd48e46659b7c35210c99f098464391a73ceb6
anuj1199 <64298418+anuj1199@users.noreply.github.com>
2020-07-12 11:42:20 +0530

    Update script.js

--- module-4-sol/script.js
+++ module-4-sol/script.js
@@ -1,6 +1,6 @@
 (function a() {
 
-	var Wish = ["Anuj", "Kartik", "Jeet", "Ankit", "Siddhant", "Jimmy", "Aditya", "Dev", "TinTin", "Jacob"];
+	var Wish = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 	for (var j = 0; j < Wish.length; j++) {
 		if (Wish[j].charAt(0).toLowerCase() === 'j') {
 			byeSpeaker.speak(Wish[j]);
